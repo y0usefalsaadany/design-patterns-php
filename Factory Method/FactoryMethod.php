fd342a7ffcfa4bf4775b4680938739418effb1ec
Yousef Alsaadany <58972656+y0usefalsaadany@users.noreply.github.com>
2022-02-10 16:11:24 +0200

    Delete Factory Method directory

--- Factory Method/FactoryMethod.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-abstract class FactoryMethod{
-  abstract function createComputer();
-  
-}
--- Factory Method/computer.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-abstract class computer{
-  public $ram = "8GB";
-  public $processor = "intel";
-  abstract function showComputer();
-
-}
