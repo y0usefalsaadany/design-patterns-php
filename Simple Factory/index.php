2da1c7e54cc34a25650dd65f52cbf8e0d9be5458
Yousef Alsaadany <58972656+y0usefalsaadany@users.noreply.github.com>
2022-02-10 16:12:02 +0200

    Delete Simple Factory directory

--- Simple Factory/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-spl_autoload_register(function ($class){
-  require($class .".php");
-});
-
-//$test = FactoryMethod::index();
-$amd = SimpleFactory::buildClass("amd");
-$intel = SimpleFactory::buildClass("intel");
--- Simple Factory/intel.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class intel{
-  public function __construct(){
-    echo "<br>no intel processor is a best<br>";
-  }
-}
